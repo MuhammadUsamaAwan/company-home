2384e14f95bc2a7622dc23311dda09f904b84f91
MuhammadUsamaAwan <underusama@gmail.com>
2025-04-10 11:58:07 +0500

    feat: nav complete

---
+++ src/components/layouts/burger.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { cn } from '~/lib/cn';
+
+type BurgerProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function Burger({ open, setOpen }: BurgerProps) {
+  return (
+    <button className={cn('block lg:hidden', !open && 'space-y-[5px]')} onClick={() => setOpen(prev => !prev)}>
+      <div className={cn('h-px bg-black duration-200 ease-in-out', open ? 'w-8 rotate-45' : 'w-[39px]')}></div>
+      <div className={cn('h-px bg-black duration-200 ease-in-out', open ? 'w-8 rotate-135' : 'ml-auto w-6')}></div>
+    </button>
+  );
+}
--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,148 +1,77 @@
+'use client';
+
+import { useRef, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Burger } from './burger';
+
 export function SiteHeader() {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <div className='relative h-screen'>
-      <header className='flex h-[150px] items-center justify-between px-[73px]'>
-        <Link href='/' className='text-[14px] font-semibold tracking-[0.2em] text-white uppercase'>
+      <header className='flex h-[70px] items-center justify-between bg-white px-[7%] md:px-[40px] lg:h-[150px] lg:bg-transparent lg:px-[73px]'>
+        <Link href='/' className='text-[14px] font-semibold tracking-[0.2em] uppercase lg:text-white'>
           Company
         </Link>
-        <nav className='flex items-center space-x-[44px]'>
-          <div className='group relative'>
-            <Link
-              className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
-              href='/'
-            >
+        <nav className='hidden items-center space-x-[44px] lg:flex'>
+          <Link
+            className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
+            href='/'
+          >
+            Home
+          </Link>
+          <Link
+            className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
+            href='/'
+          >
+            Pages
+          </Link>
+          <Link
+            className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
+            href='/'
+          >
+            Portfolio
+          </Link>
+          <Link
+            className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
+            href='/'
+          >
+            Shop
+          </Link>
+          <Link
+            className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
+            href='/'
+          >
+            Blog
+          </Link>
+        </nav>
+        <Burger open={open} setOpen={setOpen} />
+        <nav
+          ref={ref}
+          className={'absolute top-[70px] left-0 w-full overflow-hidden bg-white duration-400 lg:hidden'}
+          style={{
+            maxHeight: open ? ref.current?.scrollHeight : 0,
+          }}
+        >
+          <div className='px-[7%] pt-4.5 pb-9 md:px-[40px]'>
+            <Link href='/' className='block py-3 text-[13px] font-semibold tracking-[0.2em] uppercase'>
               Home
             </Link>
-            <div className='invisible absolute top-full left-1/2 -translate-x-1/2 translate-y-3 opacity-0 transition-all duration-300 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
-              <div className='flex flex-col gap-y-2.5 bg-white px-[38px] py-[23px]'>
-                <Link href='/' className='gradient-hover w-max'>
-                  Main Home
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  App Presentation
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Digital Services
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Product Showcase
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Company Home
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='group relative'>
-            <Link
-              className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
-              href='/'
-            >
+            <Link href='/' className='block py-3 text-[13px] font-semibold tracking-[0.2em] uppercase'>
               Pages
             </Link>
-            <div className='invisible absolute top-full left-1/2 -translate-x-1/2 translate-y-3 opacity-0 transition-all duration-300 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
-              <div className='flex flex-col gap-y-2.5 bg-white px-[38px] py-[23px]'>
-                <Link href='/' className='gradient-hover w-max'>
-                  Main Home
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  App Presentation
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Digital Services
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Product Showcase
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Company Home
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='group relative'>
-            <Link
-              className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
-              href='/'
-            >
+            <Link href='/' className='block py-3 text-[13px] font-semibold tracking-[0.2em] uppercase'>
               Portfolio
             </Link>
-            <div className='invisible absolute top-full left-1/2 -translate-x-1/2 translate-y-3 opacity-0 transition-all duration-300 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
-              <div className='flex flex-col gap-y-2.5 bg-white px-[38px] py-[23px]'>
-                <Link href='/' className='gradient-hover w-max'>
-                  Main Home
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  App Presentation
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Digital Services
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Product Showcase
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Company Home
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='group relative'>
-            <Link
-              className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
-              href='/'
-            >
+            <Link href='/' className='block py-3 text-[13px] font-semibold tracking-[0.2em] uppercase'>
               Shop
             </Link>
-            <div className='invisible absolute top-full left-1/2 -translate-x-1/2 translate-y-3 opacity-0 transition-all duration-300 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
-              <div className='flex flex-col gap-y-2.5 bg-white px-[38px] py-[23px]'>
-                <Link href='/' className='gradient-hover w-max'>
-                  Main Home
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  App Presentation
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Digital Services
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Product Showcase
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Company Home
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='group relative'>
-            <Link
-              className='gradient-hover block py-[68px] text-[13px] font-semibold tracking-[0.2em] text-white uppercase'
-              href='/'
-            >
+            <Link href='/' className='block py-3 text-[13px] font-semibold tracking-[0.2em] uppercase'>
               Blog
             </Link>
-            <div className='invisible absolute top-full left-1/2 -translate-x-1/2 translate-y-3 opacity-0 transition-all duration-300 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100'>
-              <div className='flex flex-col gap-y-2.5 bg-white px-[38px] py-[23px]'>
-                <Link href='/' className='gradient-hover w-max'>
-                  Main Home
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  App Presentation
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Digital Services
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Product Showcase
-                </Link>
-                <Link href='/' className='gradient-hover w-max'>
-                  Company Home
-                </Link>
-              </div>
-            </div>
           </div>
         </nav>
       </header>
